7547b39a35960bcc87f29cf98278a8429c56f40c
Kevin <kevin.j.dimarco@gmail.com>
2020-06-24 19:57:31 -0700

    linearLayouts are clickable and textDesc switch to match parent

--- app/src/main/java/com/dimarco/zoo/MainActivity.kt
+++ app/src/main/java/com/dimarco/zoo/MainActivity.kt
@@ -2,11 +2,16 @@ package com.dimarco.zoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.animal_ticket)
+    }
+
+    fun clickAnimal(view: View) {
+
     }
 }
