d5d1b91597a529b04768523d325adf022829ea57
Kevin <kevin.j.dimarco@gmail.com>
2020-06-24 20:11:05 -0700

    Adding click method for all the animals

--- app/src/main/java/com/dimarco/zoo/MainActivity.kt
+++ app/src/main/java/com/dimarco/zoo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dimarco.zoo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.animal_ticket.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickAnimal(view: View) {
+        when (view.id) {
+            R.id.llBaboon -> setContentView(R.layout.baboon)
+            R.id.llBulldog -> setContentView(R.layout.bulldog)
+            R.id.llPanda -> setContentView(R.layout.panda)
+            R.id.llSwallow -> setContentView(R.layout.swallow)
+            R.id.llTiger -> setContentView(R.layout.tiger)
+            R.id.llZebra -> setContentView(R.layout.zebra)
 
+        }
     }
 }
